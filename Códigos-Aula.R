4ff780049f91e40670480a27b53e7f9de9c8151a
mcristinee <mcristine2@gmail.com>
2020-06-01 15:22:09 -0300

    Create Códigos-Aula.R

---
+++ Códigos-Aula.R
@@ -0,0 +1,37 @@
+#Nessa Nossa Primeira Aula iremos entender o que o R Faz.
+
+#Ele pode funcionar como uma calculadora comum. Por exemplo:
+3+4
+4+19
+22-13.3
+3*8
+16/4
+
+#Conseguimos também fazer a raiz-quadrada, mas precisamos usar uma funçao.
+sqrt(81)
+
+# Podemos calcular potencias
+
+2^3
+12^8
+
+# Logaritimos tambem sao facilmente calculados
+log(200)
+log10(100)
+log2(20)
+
+#Todos esses calculos apareceram no terminal. Mas e se eu quiser armazenar em algum lugar pra usar depois em outro cálculo?
+#Entao eu uso um 'objeto' ou como chamado no R um vetor. 
+a = 10
+b = 12
+
+#Eu posso realizar facilmente uma operaçao com vetores
+a + b
+b * a
+
+# Posso tambem armazenar mais que 1 dado nesse vetor.
+# Mas preciso utilizar a funçao concatenar 'c()'.
+vetor1 = c(2,3,4,5,6) 
+vetor2 = 10
+
+vetor2 - vetor 1
